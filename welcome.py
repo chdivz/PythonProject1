9876e99dd9c8730355981eb0460792288540f359
Divya <divyamenon90@gmail.com>
2025-07-18 16:11:58 +0530

    Initial commit

---
+++ welcome.py
@@ -0,0 +1,12 @@
+print("Hello World");
+import sys
+print(sys.version);
+a = 15
+b = 20
+if a < b:
+    print("a is smaller than b")
+elif a>b:
+    print("a is bigger than a")
+else:
+    print("a equal to b")
+    
